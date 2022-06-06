c03eafd53d691dfe2b5d2e75d521d1167e0e9946
leizh6060 <leizh6060@yahoo.com>
2022-06-06 14:28:06 -0400

    Update doIt.groovy

--- src/codeInsrc/doIt.groovy
+++ src/codeInsrc/doIt.groovy
@@ -1,10 +1,23 @@
 package codeInsrc
 
 
-class doIt{
+//class doIt{
  
-def a_method( )
-   {
-    echo "Lei"
-   }
+//def a_method( )
+  // {
+   // echo "Lei"
+   //}
+//}
+
+class doIt {
+    def steps;
+    doIt(steps) {
+        this.steps = steps
+    }
+    def a_method() {
+        steps.echo "Hello foo from src/org/any/MultiMethod.groovy"
+    }
+    def bar() {
+        steps.echo "Hello bar from src/org/any/MultiMethod.groovy"
+    }
 }
