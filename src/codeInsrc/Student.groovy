a338b97b5ae9736c6ca7266ba435687b5235a16b
leizh6060 <leizh6060@yahoo.com>
2022-06-14 12:55:00 -0400

    Update Student.groovy

--- src/codeInsrc/Student.groovy
+++ src/codeInsrc/Student.groovy
@@ -4,11 +4,15 @@ class Student extends Person {
    int StudentID 
    int Marks1; 
 	
-   public Student() { 
-      super(); 
-   } 
+   //public Student() { 
+    //  super(); 
+   //}
+	Student(StudentID,Marks1){
+	this.StudentID = StudentID
+	this.Marks1 = Marks1
+	}
 	
-   void DisplayMarks() { 
+   def DisplayMarks(String Marks1) { 
       println(Marks1); 
    }  
 } 
