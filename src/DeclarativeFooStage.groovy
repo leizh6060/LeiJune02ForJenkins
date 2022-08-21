8d77d42ffeeec52c2d202dbece421a7e129637e0
leizh6060 <leizh6060@yahoo.com>
2022-08-20 22:44:47 -0400

    Update DeclarativeFooStage.groovy

--- src/DeclarativeFooStage.groovy
+++ src/DeclarativeFooStage.groovy
@@ -1,18 +1,18 @@
 // src/DeclarativeFooStage.groovy
 class DeclarativeFooStage {
-    private final Script script
+    private final Script1 script1
 
-    DeclarativeFooStage(Script script) {
-        this.script = script
+    DeclarativeFooStage(Script1 script1) {
+        this.script1 = script1
     }
 
     // You can pass as many parameters as needed
     void execute(String name, boolean param1) {
-        script.echo "Triggering script with name == ${name}"
-        script.sh "echo 'Execute your desired bash command here'"
+        script1.echo "Triggering script with name == ${name}"
+        script1.sh "echo 'Execute your desired bash command here'"
 
         if (param1) {
-            script.sh "echo 'Executing conditional command, because param1 == true'"
+            script1.sh "echo 'Executing conditional command, because param1 == true'"
         }
     }
 }
